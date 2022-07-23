6e0d4f0e0414e9db451606541233f95b88b4441a
Kapkowy <88287407+Kapkowy@users.noreply.github.com>
2022-07-23 23:24:47 +0200

    Here Source!

---
+++ main.py
@@ -0,0 +1,25 @@
+lol = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+pop = ['Warsaw', 'Olsztyn', 'Lublin', 'Krakow', 'Katowice', 'Wroclaw',
+       'Poznan', 'Szczecin', 'Gdansk', ' Lodz']
+
+code = input("Paste what you want to know ")
+if code[0] == "0":
+    print(pop[0])
+elif code[0] == "1":
+    print(pop[1])
+elif code[0] == "2":
+    print(pop[2])
+elif code[0] == "3":
+    print(pop[3])
+elif code[0] == "4":
+    print(pop[4])
+elif code[0] == "5":
+    print(pop[5])
+elif code[0] == "6":
+    print(pop[6])
+elif code[0] == "7":
+    print(pop[7])
+elif code[0] == "8":
+    print(pop[8])
+elif code[0] == "9":
+    print(pop[9])
